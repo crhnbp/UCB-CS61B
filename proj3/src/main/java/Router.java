817d0fb35e1fdd704a027138e8a9a66ec2858a11
YIXUAN ZHOU <zhouyixu@usc.edu>
2017-12-13 18:07:04 +0800

    Finished Proj3 Part3: Route Search

--- proj3/src/main/java/Router.java
+++ proj3/src/main/java/Router.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 /**
  * This class provides a shortestPath method for finding routes between two points
@@ -14,6 +16,52 @@ public class Router {
      * where the longs are node IDs.
      */
     public static LinkedList<Long> shortestPath(GraphDB g, double stlon, double stlat, double destlon, double destlat) {
-        return new LinkedList<Long>();
+        /** Data structures: fringe (priority queue), best (two hash maps) */
+        PriorityQueue<GraphDB.Node> fringe = new PriorityQueue<>();
+        LinkedList<Long> path = new LinkedList<>();
+        HashMap<Long, Double> distTo = new HashMap<>();
+        HashMap<Long, Long> edgeTo = new HashMap<>();
+
+        Iterable<Long> allVertices = g.vertices();
+          for (Long vertex : allVertices) {
+            distTo.put(vertex, Double.MAX_VALUE);
+        }
+
+        /** Start by adding the source to fringe. Initialize best. */
+        Long start = g.closest(stlon, stlat);
+        GraphDB.Node startNode = g.getNode(start);    // Start Node
+        startNode.setPriority(0);
+        distTo.put(start, 0.0);
+        fringe.add(startNode);
+
+        Long dest = g.closest(destlon, destlat);    // End Node
+        path.add(dest);
+
+        /** A* algorithm */
+        while (fringe.peek().getID() != dest) {
+            long curr = fringe.poll().getID();
+            for (long adj : g.adjacent(curr)) {
+                double distAdj = distTo.get(curr) + g.distance(curr, adj);
+                if (distAdj < distTo.get(adj)) {
+                    distTo.put(adj, distAdj);
+                    edgeTo.put(adj, curr);
+                    GraphDB.Node adjNode = g.getNode(adj);
+                    adjNode.setPriority(distAdj + g.distance(adj, dest));
+                    if (fringe.contains(adjNode)) {
+                        fringe.remove(adjNode);
+                        fringe.add(adjNode);
+                    } else {
+                        fringe.add(adjNode);
+                    }
+                }
+            }
+
+        }
+
+        /** path to goal */
+        for (Long edge = edgeTo.get(dest); edge != null; edge = edgeTo.get(edge)) {
+            path.addFirst(edge);
+        }
+        return path;
     }
 }
